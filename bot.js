1b9cad00de8818f7a2a5ec742567e4e662db5ae4
Juliang0705 <juliang0705@gmail.com>
2016-02-27 21:03:59 -0600

    fix bugs

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/Ellen$/;
   console.log(request)
-  if(request.text && botRegex.test(request.text)) {
+  if(request.text && botRegex.test(request.text) && request.name != "Ellen\'s Secret Admirer" ) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
