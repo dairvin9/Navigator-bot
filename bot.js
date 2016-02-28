4f8405fb656482804e8792a12a36dc678a6e5289
Juliang0705 <juliang0705@gmail.com>
2016-02-27 20:56:25 -0600

    add comments

--- bot.js
+++ bot.js
@@ -2,11 +2,34 @@ var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
-
+var niceComments = ["Are y'all talking about Ellen? She is the nicest girl I have ever met.",
+                    "Can you teach me how to have game like Ellen?",
+                    "Ellen is soooo cool",
+                    "Can you be my lunch date? Ellen",
+                    "I just wanna be like Ellen",
+                    "Did you just mention Ellen?",
+                    "Did you just mention Ellen? Are you secretly in love with her?",
+                    "I have enough Ellen for today",
+                    "@Clayton",
+                    "I like country dance too",
+                    "Ellen's bf application is still open. Apply here http://ellenstanfill.com/",
+                    "You can never be as good as Ellen",
+                    "@Ellen",
+                    "@Ellen @Clayton",
+                    "Happy Ellen day",
+                    "Knock, Knock\n Who's there?\n very long pause... \"Java\"",
+                    cool(),
+                    "Ellen is so awesome; everyone wants to be her friend.",
+                    "Don't talk to Ellen like that",
+                    "Ellen's beauty makes me stack overflow",
+                    "I want to marry a cat lady",
+                    "Ellen's beauty is illegal to have",
+                    "Ellen Ella ella eh eh eh"
+                    ]
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
-
+      botRegex = /^\/Ellen$/;
+  console.log(request)
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
@@ -21,7 +44,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = niceComments[Math.floor(Math.random() * (niceComments.length))]
 
   options = {
     hostname: 'api.groupme.com',
